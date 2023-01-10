2f6ac123bc0fcec67035e8cdf9d244cdcbd9248c
Rodrigo Pinto Pereira <rodrigo@geogis.com.br>
2023-01-09 23:04:08 -0400

    Componentes do Design System - Componente: Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam beatae tempore eveniet rerum dolorem aut voluptatum fugiat, facere quisquam dicta delectus nobis nemo? Sit sapiente et praesentium deleniti sint similique.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
