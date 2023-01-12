57f3339b89973cc28dd7336936dc126e7a754d06
Rodrigo Pinto Pereira <rodrigo@geogis.com.br>
2023-01-12 06:18:17 -0400

    Melhorias no Storybook - Controles nos componentes

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/agrodrigo.png',
     alt: 'Rodrigo Pinto Pereira',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
