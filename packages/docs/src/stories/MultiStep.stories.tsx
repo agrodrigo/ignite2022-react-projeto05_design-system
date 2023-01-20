0eb152063a21762810b1889041752bf5c2bfed16
Rodrigo Pinto Pereira <rodrigo@geogis.com.br>
2023-01-20 12:37:44 -0400

    Publicação e manutenção - Configurando Changesets

--- packages/docs/src/stories/MultiStep.stories.tsx
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import type { StoryObj, Meta } from '@storybook/react'
-import { Box, MultiStep, MultiStepProps } from '@ignite-ui/react'
+import { Box, MultiStep, MultiStepProps } from '@geogis-ignite-ui/react'
 
 export default {
   title: 'Form/Multi Step',
